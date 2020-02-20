f48d86f618c34fc68ed0638c73379b216faf54f9
Arne Engelbrecht <ArneE@discovery.co.za>
2020-02-20 20:19:29 +0200

    Add structures

---
+++ books/structures.js
@@ -0,0 +1,18 @@
+class Book {
+    constructor(id, score) {
+        this.id = id;
+        this.score = score;
+    }
+}
+
+class Library {
+    constructor(books, signupTime, processingTime) {
+        this.books = books;
+        this.signupTime = signupTime;
+        this.processingTime = processingTime;
+    }
+}
+
+
+module.exports = Book;
+module.exports = Library;
